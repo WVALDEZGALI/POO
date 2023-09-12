6a87098189fca5ac1bb138c1ac965983a1e8a4e7
WVALDEZGALI <camiloga17310@gmail.com>
2023-09-12 19:43:51 -0300

    Clases

---
+++ A_MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    //behaviors - methods
+
+    public void showName(){
+        System.out.println (name);
+    }
+}
